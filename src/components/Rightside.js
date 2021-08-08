f0f55208cb2a05de4a3ab55994d44b28859bb341
Ankouri Abdellah <ankouri120@gmai.com>
2021-08-08 11:17:38 +0000

    messanger api

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -9,7 +9,7 @@ export default function Rightside() {
     const { user } = useContext(AuthContext);
     const history = useHistory();
     const [onlineUsers, setOnlineUsers] = useState([]);
-    const socket = useRef(io("ws://socket-mern-app.herokuapp.com"));
+    const socket = useRef(io("wss://socket-mern-app.herokuapp.com"));
     
     const handleOnlineUserHome = ()=>{
         history.push('/messanger');
--- src/pages/Messanger.js
+++ src/pages/Messanger.js
@@ -60,7 +60,7 @@ export default function Messanger() {
   }
 
   useEffect(() => {
-    socket.current = io("ws://socket-mern-app.herokuapp.com");
+    socket.current = io("wss://socket-mern-app.herokuapp.com");
     socket.current.on('getMessage', data => {
       setArrivalMessages({
         sender: data.senderId,
